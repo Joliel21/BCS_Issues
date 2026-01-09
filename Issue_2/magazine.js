02491a80a9f50e86649560bc50cc8bcc1192b6b0
Joliel21 <2emailjolie@gmail.com>
2026-01-09 09:42:30 -0600

    Create magazine.js

---
+++ Issue_2/magazine.js
@@ -0,0 +1,7 @@
+// magazine.js override - UPLOADS-AWARE TEST MARKER
+(function(){
+  try{
+    console.log("[magazine_plug] OVERRIDE ACTIVE (uploads-aware magazine.js)");
+    document.documentElement.setAttribute("data-magazine-plug-override","uploads-aware-1");
+  }catch(e){}
+})();
